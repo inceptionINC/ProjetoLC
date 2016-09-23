b73363b714e383e4889c92fdb8429efffebadc53
brunopizol <brunopizolcamargo.contato@gmail.com>
2016-09-23 18:45:51 -0300

    Update Form2.cs

--- ControladorDeEstoque/ControladorDeEstoque/Form2.cs
+++ ControladorDeEstoque/ControladorDeEstoque/Form2.cs
@@ -20,15 +20,16 @@ namespace ControladorDeEstoque
         {
             InitializeComponent();
         }
+           //=====================================================================================================================
         public void CriaTabela()
         {
             try
             {
-                conexao = new MySqlConnection("server=85.10.205.173; user id=brunopizol; pwd=1q2w3e4r5t6y; database= projetolc");
+                conexao = new MySqlConnection("server=85.10.205.73; user id=brunopizol; pwd=1q2w3e4r5t6y; database= projetolc");
                 conexao.Open();
                 try
                 {
-                    comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS cadProdutos(nomeF varchar(15),categoria varchar(15), fornecedor varchar(15), dataVal DATE, descricao varchar(40),StatusF integer)", conexao);
+                    comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS cadProdutos(nomeF varchar(15),Codf varchar(9),categoria varchar(15),unidade varchar(10), fornecedor varchar(15), dataVal DATE, descricao varchar(40),StatusF integer,tipo varchar(5))", conexao);
                     comando.ExecuteNonQuery();
                 }
                 catch (Exception e)
@@ -45,6 +46,37 @@ namespace ControladorDeEstoque
             }
 
         }
+//=======================================================================================================================
+
+        public void Inserir()
+        {
+            try
+            {
+                String data = Convert.ToDateTime(maskedBox1.Text).ToString("yyyy-MM-dd");
+                conexao = new MySqlConnection("server=85.10.205.73; user id=brunopizol; pwd=1q2w3e4r5t6y; database= projetolc");
+                conexao.Open();
+                try
+                {
+                    comando = new MySqlCommand("INSERT INTO cadProdutos(NomeF, CodF, descricao,unidades, StatusF) VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','"+textBox4.Text+"',1)", conexao);
+                    MessageBox.Show(comando.CommandText);
+                    comando.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Erro no SQL");
+                }
+
+            }
+
+            catch (Exception f)
+            {
+                MessageBox.Show("Erro na conexão");
+            }
+
+
+        }
+
+//=============================================================================================================
 
         private void Form2_Load(object sender, EventArgs e)
         {
