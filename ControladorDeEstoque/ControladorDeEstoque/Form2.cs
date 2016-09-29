18f823d54ef90076c507ce025befd23b28b5ae86
brunopizol <brunopizolcamargo.contato@gmail.com>
2016-09-29 18:46:23 -0300

    Update Form2.cs

--- ControladorDeEstoque/ControladorDeEstoque/Form2.cs
+++ ControladorDeEstoque/ControladorDeEstoque/Form2.cs
@@ -25,11 +25,11 @@ namespace ControladorDeEstoque
         {
             try
             {
-                conexao = new MySqlConnection("server=85.10.205.73; user id=brunopizol; pwd=1q2w3e4r5t6y; database= projetolc");
+                conexao = new MySqlConnection("server=db4free.net; user id= brunopizol; pwd= 1q2w3e4r5t6y; database= projetolc");
                 conexao.Open();
                 try
                 {
-                    comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS cadProdutos(nomeF varchar(15),Codf varchar(9),categoria varchar(15),unidade varchar(10), fornecedor varchar(15), dataVal DATE, descricao varchar(40),StatusF integer,tipo varchar(5),CodBarras varchar(256),EstMax varchar(10),EstMin varchar(10))", conexao);
+                    comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS cadProdutos(nomeF varchar(15),Codf varchar(9),categoria varchar(15),unidade varchar(10), fornecedor varchar(40), dataVal DATE, descricao varchar(40),StatusF integer,tipo varchar(5),CodBarras varchar(256),EstMax varchar(10),EstMin varchar(10),peso varchar(15))", conexao);
                     comando.ExecuteNonQuery();
                 }
                 catch (Exception e)
@@ -53,11 +53,11 @@ namespace ControladorDeEstoque
             try
             {
                 String data = Convert.ToDateTime(maskedTextBox1.Text).ToString("yyyy-MM-dd");
-                conexao = new MySqlConnection("server=85.10.205.73; user id=brunopizol; pwd=1q2w3e4r5t6y; database= projetolc");
+                conexao = new MySqlConnection("server=db4free.net; user id=brunopizol; pwd=1q2w3e4r5t6y; database= projetolc");
                 conexao.Open();
                 try
                 {
-                    comando = new MySqlCommand("INSERT INTO cadProdutos(NomeF, CodF, descricao,unidades,categoria,dataVal,peso, tipo,fornecedor,EstMin, EstMax,CodBarras, StatusF) VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','"+comboBox2.Text+"','"+data+"','"+textBox5.Text+"','"+comboBox3.Text+"','"+comboBox1.Text+"','"+ numericUpDown1.Text+"','"+ numericUpDown2.Text+"','"+textBox8.Text+"',1)", conexao);
+                    comando = new MySqlCommand("INSERT INTO cadProdutos(NomeF, CodF, descricao,unidade,categoria,dataVal,peso, tipo,fornecedor,EstMin, EstMax,CodBarras, StatusF) VALUES('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','"+comboBox2.Text+"','"+data+"','"+textBox5.Text+"','"+comboBox3.Text+"','"+comboBox1.Text+"','"+ numericUpDown1.Text+"','"+ numericUpDown2.Text+"','"+textBox8.Text+"',1)", conexao);
                     //MessageBox.Show(comando.CommandText);
                     MessageBox.Show("Produtos cadastrados com sucesso!");
                     comando.ExecuteNonQuery();
@@ -78,6 +78,21 @@ namespace ControladorDeEstoque
         }
 
         //=============================================================================================================
+        public void LimpaCampos()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            //comboBox1.SelectedIndex = 0;
+           // comboBox2.SelectedIndex = 0;
+            //comboBox3.SelectedIndex = 0;
+            maskedTextBox1.Clear();
+        }
 
         private void Form2_Load(object sender, EventArgs e)
         {
@@ -90,6 +105,7 @@ namespace ControladorDeEstoque
         private void button1_Click(object sender, EventArgs e)
         {
             Inserir();
+            LimpaCampos();
         }
     }
 }
